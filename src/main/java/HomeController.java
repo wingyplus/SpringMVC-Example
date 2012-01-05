b2c70379d20e4cc6ea6c3ef671261cfc5604d935
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2012-01-05 13:01:08 +0700

    delete target build \ create service comparator \ action can input from GET method \ config pom.xml

--- src/main/java/HomeController.java
+++ src/main/java/HomeController.java
@@ -1,15 +1,38 @@
 package xyz.sample.baremvc;
 
 import java.util.Date;
+import java.util.Comparator;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+
 
 @Controller
 public class HomeController {
+    @Autowired
+    Comparator<String> comparator;
 
-    @RequestMapping(value="/")
+    @RequestMapping(value = "/")
     public String home() {
     	System.out.println("HomeController: Passing through..." + new Date());
     	return "home";
+    }
+
+    @RequestMapping(value = "/compare", method = RequestMethod.GET)
+    public String compare(@RequestParam("input1") String input1, 
+            @RequestParam("input2") String input2, Model model) {
+
+        int result = comparator.compare(input1, input2);
+        String inEnglish = (result < 0) ? "less than" : (result > 0 ? "greather than" : "equal to");
+        String output = "Accoding to our Comparator, '" + input1 + "' is " + inEnglish + "'" + input2 + "'";
+
+        model.addAttribute("output", output);
+        
+        // bind view 'compareResult'
+        return "compareResult";
     }
 }
