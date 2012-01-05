f073cad592442461e28413370ca72b044a6af52f
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2012-01-05 10:33:41 +0700

    config mapping view by specific prefix and suffix

---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,17 @@
+package xyz.simple.baremvc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
