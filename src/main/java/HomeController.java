093f4ba3dfc9d2b98248e44ca53b4e7c3f39bbfc
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2012-01-05 10:13:56 +0700

    first commit

---
+++ src/main/java/HomeController.java
@@ -0,0 +1,15 @@
+package xyz.sample.baremvc;
+
+import java.util.Date;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value="/")
+    public String home() {
+    	System.out.println("HomeController: Passing through..." + new Date());
+    	return "WEB-INF/views/home.jsp";
+    }
+}
