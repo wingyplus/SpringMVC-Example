3e28ae53e2dcaddfb5d05ca29ad0e466a740166e
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2012-01-06 13:33:10 +0700

    move method /mapping to MappingController class

---
+++ src/main/java/MappingController.java
@@ -0,0 +1,46 @@
+package xyz.sample.baremvc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+@RequestMapping(value = "/mapping")
+public class MappingController {
+	
+	@RequestMapping(value = "/path")
+	public @ResponseBody String byPath() {
+		return "Mapping by Path";
+	}
+	
+	@RequestMapping(value = "/method", method = RequestMethod.GET)
+	public @ResponseBody String byParameterAll() {
+		return "Mapping by Method All";
+	}
+	
+	// mapping parameter name foo
+	@RequestMapping(value = "/parameter", method = RequestMethod.GET, params = "foo")
+	public @ResponseBody String byParameter() {
+		return "Mapping by Parameter foo";
+	}
+	
+	@RequestMapping(value = "/parameter", method = RequestMethod.GET, params = "!foo")
+	public @ResponseBody String byParameterNegation() {
+		return "Mapping by Parameter (all parameter not foo)";
+	}
+	
+	@RequestMapping(value = "/consumes", method = RequestMethod.POST, consumes = "application/json")
+	public @ResponseBody String byConsumes(@RequestBody JavaBean javaBean) {
+		return "Mapping By path + method + consumable media type (JavaBean'" + javaBean + "')";
+	}
+	
+	@RequestMapping(value = "/produces", method = RequestMethod.GET, produces = "application/json")
+	public @ResponseBody JavaBean byProduces() {
+		return new JavaBean();
+	}
+}
